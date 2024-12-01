a0687c0c3a5719e96b2bc37d913bf0a217a4c868
ashishmaurya-crypto <ashishmaurya7547@gmail.com>
2024-12-01 12:21:39 +0530

    add socket

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const socket = require('socket.io')
 const { app } = require('./app');
 require('./src/db/db').connectDatabase()
 
@@ -8,26 +7,3 @@ require('./src/db/db').connectDatabase()
 const port = process.env.PORT;
 console.log('server start @', port)
 app.listen(port);
-
-// const io = socket(app.listen(port), {
-//     cors: {
-//         origin : 'http://localhost:4000',
-//         credentials : true,
-//     },
-// });
-
-// global.onlineUsers = new Map();
-
-// io.on("connection", (socket)=>{
-//     global.chatSocket = socket;
-//     socket.on("add-user", (userId)=> {
-//         onlineUsers.set(userId, socket.id);
-//     })
-//     socket.on("send-msg", (data)=> {
-//         const sendUserSocket = onlineUsers.get(data.io);
-//         if(sendUserSocket){
-//             socket.io(sendUserSocket).emit('msg-recieve', data.msg)
-//         }
-//     })
-
-// })
